d4c70869cc0ab36707cfa5c7ebdaeb8ba707b6af
Wendy Beck <webeck@gmail.com>
2015-01-21 16:44:32 -0800

    Switching blog page to use a json obj to display all posts

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,27 @@ var router = express.Router();
 
 var posts = require('../posts');
 
-router.param('post_name', function(request, response, next, post_name){
-    //note: we'll define posts.find in a minute
-    posts.find(post_name, function(post) {
-        if(post) {
-            request.post = post;
-            next();
-        } else {
+// router.param('post_name', function(request, response, next, post_name){
+//     //note: we'll define posts.find in a minute
+//     posts.find(post_name, function(post) {
+//         if(post) {
+//             request.post = post;
+//             request.title = post_name;
+//             next();
+//         } else {
 
-        }
-    });
-});
+//         }
+//     });
+// });
+
+
+
 router.get('/', function(req, res) {
   res.render('index', { title: 'My Freaking Fantastic Blog!' });
 });
 
 router.get('/post/:post_name', function(request, response) {
-    response.render('post', {post: request.post});
+    response.render('post', {post: request.post, postTitle:request.title});
 });
 
 
